ce34a5424a787e548e461a58a907f06760df8603
James Sessford <hello@jamessessford.com>
2021-02-16 06:55:23 +0000

    wip

--- src/IsAudited.php
+++ src/IsAudited.php
@@ -5,7 +5,7 @@ namespace Rrvwmrrr\Auditor;
 trait IsAudited {
     public static function bootIsAudited() {
         static::creating(function($model) {
-
+            dd($model);
         });
     }
 }
