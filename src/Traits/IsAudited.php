b64e1f0c7b706d903e96de227c7180653dbfe038
James Sessford <hello@jamessessford.com>
2021-02-16 09:27:19 +0000

    wip

--- src/Traits/IsAudited.php
+++ src/Traits/IsAudited.php
@@ -2,30 +2,26 @@
 
 namespace Rrvwmrrr\Auditor\Traits;
 
+use Illuminate\Support\Facades\Auth;
 use Rrvwmrrr\Auditor\Audit;
 
 trait IsAudited {
     public static function bootIsAudited() {
         static::creating(function($model) {
             $auditData = [
-                'auditable_id' => $model->id,
+                'auditable_id' => $model->id ?? 0,
                 'auditable_type' => get_class($model),
                 'event' => 'Create',
                 'changes' => $model->toJson(),
             ];
 
-            dd($auditData);
-            
-            Audit::create([
-                'auditable_id' => $model->id,
-                'auditable_type' => get_class($model),
-                'event' => 'Create',
-                'changes' => $model->toJson(),
-                'auditor_id' => Auth::user()->id,
-                'auditor' => config('little-auditor.auditor_model')
-            ]);
-            
-            dd($model);
+            $user = optional(Auth::user()->id);
+            if ($user) {
+                $auditData['auditor_id'] = $user;
+                $auditData['auditor_type'] = config('little-auditor.auditor_model');
+            }
+
+            Audit::create($auditData);
         });
     }
 
