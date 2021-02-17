8bd5d64ff272d9640e062d9120e24d09f55414a4
James Sessford <hello@jamessessford.com>
2021-02-17 08:38:40 +0000

    wip

--- src/Traits/IsAuditor.php
+++ src/Traits/IsAuditor.php
@@ -5,15 +5,7 @@ namespace Rrvwmrrr\Auditor\Traits;
 use Rrvwmrrr\Auditor\Audit;
 
 trait IsAuditor {
-    public static function bootIsAuditor() {
-        
-    }
-
     public function audits() {
         return $this->morphMany(Audit::class, 'auditorable');
-    }
-
-    public function type() {
-        return static::class;
     }
 }
